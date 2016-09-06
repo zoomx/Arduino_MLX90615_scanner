c24ed4c3dce07bfb9c69ffa5fefc7abb7f03f5f5
zoomx <zoomx@tiscalinet.it>
2016-09-06 12:03:12 +0200

    Add files via upload
    VB.NET first version

---
+++ VB.NET sources/PanTiltScanner/fMain.Designer.vb
@@ -0,0 +1,180 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fMain
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.comPortComboBox = New System.Windows.Forms.ComboBox()
+        Me.connectDisconnectButton = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ToolStripStatusLabel2 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.bEnd = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.bScan = New System.Windows.Forms.Button()
+        Me.ToolStripStatusLabel3 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.bNormalize = New System.Windows.Forms.Button()
+        Me.bTest = New System.Windows.Forms.Button()
+        Me.StatusStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'comPortComboBox
+        '
+        Me.comPortComboBox.FormattingEnabled = True
+        Me.comPortComboBox.Location = New System.Drawing.Point(12, 12)
+        Me.comPortComboBox.Name = "comPortComboBox"
+        Me.comPortComboBox.Size = New System.Drawing.Size(99, 21)
+        Me.comPortComboBox.Sorted = True
+        Me.comPortComboBox.TabIndex = 15
+        '
+        'connectDisconnectButton
+        '
+        Me.connectDisconnectButton.Location = New System.Drawing.Point(12, 39)
+        Me.connectDisconnectButton.Name = "connectDisconnectButton"
+        Me.connectDisconnectButton.Size = New System.Drawing.Size(99, 21)
+        Me.connectDisconnectButton.TabIndex = 17
+        Me.connectDisconnectButton.Text = "&Connect"
+        Me.connectDisconnectButton.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1, Me.ToolStripStatusLabel2, Me.ToolStripStatusLabel3})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 236)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(298, 24)
+        Me.StatusStrip1.TabIndex = 18
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(43, 19)
+        Me.ToolStripStatusLabel1.Text = "Status"
+        '
+        'ToolStripStatusLabel2
+        '
+        Me.ToolStripStatusLabel2.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right
+        Me.ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        Me.ToolStripStatusLabel2.Size = New System.Drawing.Size(55, 19)
+        Me.ToolStripStatusLabel2.Text = "Nothing"
+        '
+        'bEnd
+        '
+        Me.bEnd.Location = New System.Drawing.Point(136, 101)
+        Me.bEnd.Name = "bEnd"
+        Me.bEnd.Size = New System.Drawing.Size(50, 25)
+        Me.bEnd.TabIndex = 19
+        Me.bEnd.Text = "&End"
+        Me.bEnd.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 76)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 20
+        Me.PictureBox1.TabStop = False
+        '
+        'bScan
+        '
+        Me.bScan.Location = New System.Drawing.Point(136, 12)
+        Me.bScan.Name = "bScan"
+        Me.bScan.Size = New System.Drawing.Size(54, 34)
+        Me.bScan.TabIndex = 21
+        Me.bScan.Text = "&Scan"
+        Me.bScan.UseVisualStyleBackColor = True
+        '
+        'ToolStripStatusLabel3
+        '
+        Me.ToolStripStatusLabel3.Name = "ToolStripStatusLabel3"
+        Me.ToolStripStatusLabel3.Size = New System.Drawing.Size(36, 19)
+        Me.ToolStripStatusLabel3.Text = "Value"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(205, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 22
+        '
+        'bNormalize
+        '
+        Me.bNormalize.Location = New System.Drawing.Point(136, 52)
+        Me.bNormalize.Name = "bNormalize"
+        Me.bNormalize.Size = New System.Drawing.Size(67, 27)
+        Me.bNormalize.TabIndex = 23
+        Me.bNormalize.Text = "&Normalize"
+        Me.bNormalize.UseVisualStyleBackColor = True
+        '
+        'bTest
+        '
+        Me.bTest.Location = New System.Drawing.Point(192, 101)
+        Me.bTest.Name = "bTest"
+        Me.bTest.Size = New System.Drawing.Size(67, 27)
+        Me.bTest.TabIndex = 24
+        Me.bTest.Text = "&Fill test"
+        Me.bTest.UseVisualStyleBackColor = True
+        '
+        'fMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 260)
+        Me.Controls.Add(Me.bTest)
+        Me.Controls.Add(Me.bNormalize)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.bScan)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.bEnd)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.connectDisconnectButton)
+        Me.Controls.Add(Me.comPortComboBox)
+        Me.Name = "fMain"
+        Me.Text = "Pan Tilt Scanner"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents comPortComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents connectDisconnectButton As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents bEnd As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents bScan As System.Windows.Forms.Button
+    Friend WithEvents ToolStripStatusLabel3 As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents bNormalize As System.Windows.Forms.Button
+    Friend WithEvents bTest As System.Windows.Forms.Button
+
+End Class
---
+++ VB.NET sources/PanTiltScanner/fMain.vb
@@ -0,0 +1,285 @@
+﻿'Imports System.IO
+
+Public Class fMain
+    Public Connected As Boolean = False
+    Public TiltCenter As Integer
+    Public PanCenter As Integer
+    Public ServoMin As Integer
+    Public ServoMax As Integer
+    Public PanMin As Integer = 75
+    Public PanMax As Integer = 150
+    Public TiltMin As Integer = 75
+    Public TiltMax As Integer = 150
+    Public ValueMin As Integer = 0
+    Public ValueMax As Integer = 255
+    Public ImageWidth As Integer
+    Public ImageHeight As Integer
+    Public Stringa As String
+    Public Pixels(300, 300) As Integer
+    Public PixelMax As Integer
+    Public PixelMin As Integer
+
+
+    Private Sub fMain_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        updateComPortList()
+        ToolStripStatusLabel1.Text = "Serial Port Closed"
+        'SwitchToDegrees()
+        Debug.Print("Start!")
+        Debug.Print("Start!")
+        ImageWidth = PanMax - PanMin
+        ImageHeight = TiltMax - TiltMin
+        PictureBox1.Width = ImageWidth + 1
+        PictureBox1.Height = ImageHeight + 1
+        bScan.Enabled = False
+
+    End Sub
+    Private Sub updateComPortList()
+        If connectDisconnectButton.Text = "&Connect" Then
+            comPortComboBox.Items.Clear()
+            comPortComboBox.Items.Add("None")
+
+            Dim s As String
+            For Each s In IO.Ports.SerialPort.GetPortNames()
+                comPortComboBox.Items.Add(s)
+            Next s
+
+            comPortComboBox.SelectedIndex = 0
+            'baudRateComboBox.SelectedIndex = 0
+        End If
+    End Sub
+    Private Sub connectDisconnectButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles connectDisconnectButton.Click
+
+        If connectDisconnectButton.Text = "&Connect" Then
+            If comPortOpen() Then
+                connectDisconnectButton.Text = "&Disconnect"
+                bScan.Enabled = True
+                'Ferma = False
+                'GetData()
+            Else
+                'TextBox.AppendText("Error: Unable to open specified Com Port")
+            End If
+        Else
+            'Ferma = True
+            'SerialPort1.Close()
+            ToolStripStatusLabel1.Text = "Serial Port Closed"
+            connectDisconnectButton.Text = "&Connect"
+        End If
+    End Sub
+    Private Function comPortOpen() As Boolean
+        Dim portName As String = Nothing
+        Dim baudRate As Integer
+
+        portName = comPortComboBox.Text
+        baudRate = Convert.ToInt32(115200)
+
+        comPortOpen = True
+
+        Try
+
+            With SerialPort1
+                .BaudRate = baudRate
+                .PortName = portName
+                .DataBits = 8
+                .Parity = IO.Ports.Parity.None
+                .StopBits = IO.Ports.StopBits.One
+                .Encoding = System.Text.Encoding.Default
+                '.ReceivedBytesThreshold = 1
+                '.ReadTo(vbCrLf)
+                .Handshake = IO.Ports.Handshake.None
+                .Open()
+            End With
+            Connected = True
+        Catch ex As Exception
+            ToolStripStatusLabel1.Text = "Unable to open"
+            Connected = False
+            Return False
+
+        End Try
+        'portName = comPortComboBox.Text
+        'baudRate = Convert.ToInt32(baudRateComboBox.Text)
+
+        'textBox.AppendText("Port Name :" & portName & vbCrLf)
+        'textBox.AppendText("Baud Rate :" & baudRate & vbCrLf)
+
+        'SerialPort1.PortName = portName
+        'SerialPort1.BaudRate = baudRate
+        'SerialPort1.Open()
+
+        'If SerialPort1.IsOpen Then
+        '    textBox.AppendText("Serial Port Opened")
+        '    textBox.AppendText(Environment.NewLine)
+        '    Return True
+        'End If
+
+        'TextBox.AppendText("Serial Port Opened")
+        ToolStripStatusLabel1.Text = "Serial Port Opened"
+        'TextBox.AppendText(Environment.NewLine)
+        Return True
+    End Function
+    Private Sub comPortComboBox_DropDown(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles comPortComboBox.DropDown
+        updateComPortList()
+    End Sub
+    Private Sub SendPanTilt(Pan As Integer, Tilt As Integer)
+        Dim message As String
+
+        'Exit Sub
+
+        message = Pan
+        message &= ","
+        message &= Tilt
+        'ToolStripStatusLabel2.Text = message
+
+        message = Chr(CInt(Pan))
+        message &= Chr(CInt(Tilt))
+        message &= Chr(255)
+
+
+        If Connected = True Then
+            SerialPort1.Write(message)
+        End If
+    End Sub
+    'Private Sub SerialPort1_DataReceived(sender As Object, e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+    '    Dim stringa As String
+    '    stringa = SerialPort1.ReadExisting
+    '    'SerialRX.Text &= stringa
+    '    'If SerialRX.
+    '    Debug.Print(stringa)
+
+    'End Sub
+
+    Private Sub bEnd_Click(sender As System.Object, e As System.EventArgs) Handles bEnd.Click
+        If Connected = True Then
+            SerialPort1.Close()
+        End If
+        If SerialPort1.IsOpen = True Then
+            SerialPort1.Close()
+        End If
+        End
+    End Sub
+
+    Private Sub bScan_Click(sender As System.Object, e As System.EventArgs) Handles bScan.Click
+        Dim Pan, Tilt, x, y As Integer
+        Dim value As Integer
+        Dim Start As Date
+        Dim Stopp As Date
+        Dim TimeElapsed As System.TimeSpan
+        PixelMax = 0
+        PixelMin = 255
+
+        Start = Now
+
+        Label1.Text = "Scanning"
+        SerialPort1.DiscardInBuffer()
+        'PictureBox1.Image = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Dim Bitmappa = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        For Pan = PanMin To PanMax
+            For Tilt = TiltMin To TiltMax
+                SendPanTilt(Pan, Tilt)
+                'value = Pan + Tilt
+                value = SerialPort1.ReadByte
+                x = Pan - PanMin
+                y = Tilt - TiltMin
+                If value > PixelMax Then
+                    PixelMax = value
+                End If
+
+                If value < PixelMin Then
+                    PixelMin = value
+
+                End If
+                Pixels(Pan, Tilt) = value
+
+                'Bitmap.SetPixel(Pan, Tilt, Color.FromArgb(255, 255, 0))
+                'PictureBox1.Image.
+                'DirectCast(PictureBox1.Image, Bitmap).SetPixel(x, y, Color.FromArgb(value, 0, 0))
+                Bitmappa.SetPixel(x, y, Color.FromArgb(value, 0, 0))
+                'ToolStripStatusLabel3.Text = value.ToString
+                PictureBox1.Image = Bitmappa
+            Next
+        Next
+        'PictureBox1.Refresh()
+        PictureBox1.Image = Bitmappa
+        Stopp = Now
+        TimeElapsed = Stopp - Start
+        ToolStripStatusLabel3.Text = TimeElapsed.ToString
+        Label1.Text = "Done!"
+    End Sub
+
+    Private Sub bNormalize_Click(sender As System.Object, e As System.EventArgs) Handles bNormalize.Click
+        Dim pan, tilt, x, y As Integer
+        Dim m As Single
+        Dim q As Integer
+        Dim Start As Date
+        Dim Stopp As Date
+        Dim TimeElapsed As System.TimeSpan
+        Dim Bitmappa = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Dim oldvalue As Integer
+        Dim range As Integer
+
+        Label1.Text = "Normalizing"
+        Start = Now
+        m = 255 / (PixelMax - PixelMin)
+        q = (-PixelMin * 255) / (PixelMax - PixelMin)
+        range = PixelMax - PixelMin
+        For pan = PanMin To PanMax
+            For tilt = TiltMin To TiltMax
+                oldvalue = Pixels(pan, tilt)
+                'Pixels(pan, tilt) = CInt(Pixels(pan, tilt) * m - PixelMin)
+                'Pixels(pan, tilt) = CInt(Pixels(pan, tilt) * m - q)
+                Pixels(pan, tilt) = CInt((Pixels(pan, tilt) - PixelMin) / range * 255)
+                x = pan - PanMin
+                y = tilt - TiltMin
+                Bitmappa.SetPixel(x, y, Color.FromArgb(Pixels(pan, tilt), 0, 0))
+            Next
+        Next
+
+        PictureBox1.Image = Bitmappa
+        Stopp = Now
+        TimeElapsed = Stopp - Start
+        ToolStripStatusLabel3.Text = TimeElapsed.ToString
+        Label1.Text = "Done!"
+    End Sub
+
+    Private Sub bTest_Click(sender As System.Object, e As System.EventArgs) Handles bTest.Click
+        Dim Pan, Tilt, x, y As Integer
+        Dim value As Integer
+        Dim Start As Date
+        Dim Stopp As Date
+        Dim TimeElapsed As System.TimeSpan
+        PixelMax = 0
+        PixelMin = 255
+
+        Start = Now
+
+        Label1.Text = "Filling"
+        'PictureBox1.Image = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Dim Bitmappa = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        For Pan = PanMin To PanMax
+            For Tilt = TiltMin To TiltMax
+                value = Pan
+                x = Pan - PanMin
+                y = Tilt - TiltMin
+                If value > PixelMax Then
+                    PixelMax = value
+                End If
+                If value < PixelMin Then
+                    PixelMin = value
+                End If
+                Pixels(Pan, Tilt) = value
+
+                'Bitmap.SetPixel(Pan, Tilt, Color.FromArgb(255, 255, 0))
+                'PictureBox1.Image.
+                'DirectCast(PictureBox1.Image, Bitmap).SetPixel(x, y, Color.FromArgb(value, 0, 0))
+                Bitmappa.SetPixel(x, y, Color.FromArgb(value, 0, 0))
+                'ToolStripStatusLabel3.Text = value.ToString
+                PictureBox1.Image = Bitmappa
+            Next
+        Next
+        'PictureBox1.Refresh()
+        PictureBox1.Image = Bitmappa
+        Stopp = Now
+        TimeElapsed = Stopp - Start
+        ToolStripStatusLabel3.Text = TimeElapsed.ToString
+        Label1.Text = "Done!"
+    End Sub
+End Class
